ad71fdee6d5c9ce3f0e1eb8520acb9d2611bf850
adilraees786 <adilraees911@gmail.com>
2024-11-15 16:20:00 +0500

    first

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+
+
+export default function Home(){
+  return(
+    <div>
+      <h1 className=" mb-5"> Adil Raees</h1>
+      <Button variant="destructive">Destructive</Button>
+    </div>
+  )
+}
