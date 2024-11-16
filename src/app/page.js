945d191e206f235fbc689d80ed2d6dbf65970b56
adilraees786 <adilraees911@gmail.com>
2024-11-16 19:20:42 +0500

    changetext

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button"
 export default function Home(){
   return(
     <div>
-      <h1 className=" mb-5"> Adil Raees</h1>
+      <h1 className=" mb-5 text-xl"> Adil Raees</h1>
       <Button variant="destructive">Button</Button>
     </div>
   )
