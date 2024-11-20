f448082f221447eaff6b64093c3266ad657f43e6
adilraees786 <adilraees911@gmail.com>
2024-11-20 16:01:45 +0500

    addcontainer

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div>
+    <div className=" container">
       <div>
         <h1 className=" mb-5 text-2xl font-extrabold" > Todo List</h1>
         <input type="text"/>
