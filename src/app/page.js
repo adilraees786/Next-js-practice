021a1ab8d1e77e8099d7fffbec95db6baac254a0
adilraees786 <adilraees911@gmail.com>
2024-11-21 19:09:22 +0500

    add bg

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div className=" container flex">
+    <div className=" container bg-slate-50">
       <div>
         <h1 className=" mb-5 text-2xl font-extrabold" > Todo List</h1>
         <input type="text"/>
